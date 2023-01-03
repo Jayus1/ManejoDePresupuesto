f6f9409b253f1c097ff11d86c94d2668107df774
Julio Sierra <juliosierra299@gmail.com>
2023-01-02 21:59:27 -0400

    Capitulo 124

---
+++ ManejoPresupuesto/Controllers/CuentasController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ManejoPresupuesto.Controllers
+{
+    public class CuentasController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
